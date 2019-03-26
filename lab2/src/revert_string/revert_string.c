30ea087538ef03ce0bc0e4a8508c9b0381d05a5b
MaximTermosa <formulabernulli@gmail.com>
2019-03-26 15:38:34 +0000

    done

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,14 @@
 #include "revert_string.h"
+#include "string.h"
 
 void RevertString(char *str)
 {
-	// your code here
+    int len = strlen(str);
+    char revStr[len];
+	for(int i = 0; i < len; i++)
+	{
+	    revStr[i] = str[len-i-1];
+	}
+	strcpy(str, revStr);
 }
 
